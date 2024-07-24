a798714ab093b229d132e0d987bd6cf09c9c5d54
Sungju Kim <sjkim20010909@gmail.com>
2024-07-24 14:19:44 +0900

    step1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,20 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+
+        Scanner scanner = new Scanner(System.in);
+        String nb1;
+        String nb2;
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        nb1 = scanner.nextLine();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        nb2 = scanner.nextLine();
+
+        System.out.println("nb1: "+nb1+" nb2: "+nb2);
     }
 }
