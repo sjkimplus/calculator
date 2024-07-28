a330d508c046db0b85d684469d0ab340ff2e4591
Sungju Kim <sjkim20010909@gmail.com>
2024-07-28 23:07:36 +0900

    lv2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,4 @@
 package calculator;
-import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class App {
@@ -11,43 +10,60 @@ public class App {
         String nb1;
         String nb2;
         String op;
-        boolean play = true;
+        int play = 0;
         String again;
         String remove;
         String inquiry;
         Calculator myCalc = new Calculator();
 
-        while(play) {
-            // getting the numbers and operator
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            nb1 = scanner.nextLine();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            nb2 = scanner.nextLine();
+        System.out.print("사칙연산을 원하면 '1' \n원의 너비를 원하면 '2'를 입력해 주세요: ");
+        play = Integer.parseInt(scanner.nextLine());
+        while(play > 0) {
+            if (play == 1)
+            {
+                double calcResult;
+                // getting the numbers and operator
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                nb1 = scanner.nextLine();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                nb2 = scanner.nextLine();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            op = scanner.nextLine();
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                op = scanner.nextLine();
 
-            // performing the operation
-            BigDecimal nb11 = new BigDecimal(nb1);
-            BigDecimal nb22 = new BigDecimal(nb2);
+                // performing the operation
+                double nb11 = Double.parseDouble(nb1);
+                double nb22 = Double.parseDouble(nb2);
 
-            myCalc.calculate(nb11, nb22, op);
+                calcResult = myCalc.calculate(nb11, nb22, op);
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제, enter 누를시 다음)");
-            remove = scanner.nextLine();
-            if (remove.equals("remove"))
-                myCalc.removeResult();
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회, enter 누를시 다음)");
-            inquiry = scanner.nextLine();
-            if (inquiry.equals("inquiry")) {
+                // print result
+                System.out.println("계산결과: "+ String.valueOf(calcResult));
+                // save result
+                myCalc.setResult(calcResult);
+                // inquire
                 myCalc.inquiryResults();
+            }
+            else if (play==2)
+            {
+                double radius;
+                double circleResults;
+                System.out.print("반지름의 크기를 입력하시오: ");
+                radius = Double.parseDouble(scanner.nextLine());
+                circleResults = myCalc.calculateCircleArea(radius);
+
+                // print results
+                System.out.println("계산결과: "+ String.valueOf(circleResults));
+                // save results
+                myCalc.setCircleArea(circleResults);
+                // inquire
+                myCalc.inquiryCircleResults();
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, enter 누를시 지속)");
             again = scanner.nextLine();
             if (again.equals("exit"))
-                play = false;
+                play = -1;
         }
         scanner.close();
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,36 +1,38 @@
 package calculator;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class Calculator {
 
     private Queue<String> arch;
+    private Queue<String> archCircle;
+    // 파이의 값은 절때 바뀌지 않는 상수라 final을 사용 했습니다.
+    final double pi = 3.14159265358979323846264338;
 
     Calculator () {
         arch = new LinkedList<>();
+        archCircle = new LinkedList<>();
     }
 
-    public BigDecimal calculate(BigDecimal nb1, BigDecimal nb2, String op) {
-        BigDecimal result=BigDecimal.ZERO;
+    public double calculate(double nb1, double nb2, String op) {
+        double result = 0;
         try
         {
             switch (op) {
                 case "+":
-                    result = nb1.add(nb2);
+                    result = nb1 + nb2;
                     break;
                 case "-":
-                    result = nb1.subtract(nb2);
+                    result = nb1 - nb2;
                     break;
                 case "*":
-                    result = nb1.multiply(nb2);
+                    result = nb1 * nb2;
                     break;
                 case "/":
-                    if (nb2.equals(BigDecimal.ZERO)) {
+                    if (nb2==0) {
                         throw new Exception("0 으로 나눌 수 없음.");
                     }
-                    result = nb1.divide(nb2, 5, RoundingMode.HALF_UP);
+                    result = nb1/nb2;
                     break;
                 default:
                     throw new Exception("에러: 잘못된 연산자입니다.");
@@ -41,7 +43,8 @@ public class Calculator {
             System.out.println("Error: " + e.getMessage());
             return result;
         }
-        setResult(String.valueOf(result));
+
+        // print results
         System.out.println("계산결과: "+ String.valueOf(result));
         return result;
     }
@@ -52,8 +55,8 @@ public class Calculator {
     }
 
     // Setter Method: adds new method into the
-    public void setResult(String result) {
-        arch.add(result);
+    public void setResult(double result) {
+        arch.add(String.valueOf(result));
     }
 
     public void removeResult() {
@@ -64,5 +67,27 @@ public class Calculator {
         for (String result : arch)
             System.out.println(result);
     }
+
+    /* 원의 넓이를 구하는 메서드 선언*/
+    public double calculateCircleArea(double r) {
+        double circleArea = pi*r*r;
+        return circleArea;
+    }
+
+    // 원넓이 getter method
+    public String getCircleArea() {
+        return archCircle.peek();
+    }
+
+    // 원넓이 setter method
+    public void setCircleArea(double circleArea) {
+        archCircle.add(String.valueOf(circleArea));
+    }
+
+    public void inquiryCircleResults() {
+        for (String result : archCircle)
+            System.out.println(result);
+    }
+
 
 }
